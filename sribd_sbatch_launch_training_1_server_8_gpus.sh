b6123f2270033acc460d7e50e964d97775e49eb5
zhengyang <zhengyang>
2023-04-02 21:25:19 +0800

    xxx

--- sribd_sbatch_launch_training_1_server_8_gpus.sh
+++ sribd_sbatch_launch_training_1_server_8_gpus.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=hej0b
+#SBATCH --job-name=tangzhengyang
+#SBATCH -p p-V100
 #SBATCH --time=14-00:00:00
 #SBATCH -N 1
 #SBATCH --mem=64GB
 #SBATCH --ntasks-per-node=8
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:8
-#SBATCH --reservation=A100
 time=$(date "+%m%d-%H%M")
 
 python -m torch.distributed.launch \
   --nproc_per_node 8 \
-  /ibex/user/hej0b/zhengyang/PseudoTrain/run_glue.py \
+  /home/tangzhengyang/PseudoTrain/run_glue.py \
   --model_name_or_path bert-base-cased \
   --task_name mrpc \
   --do_train \
@@ -20,6 +20,7 @@ python -m torch.distributed.launch \
   --per_device_train_batch_size 32 \
   --learning_rate 2e-5 \
   --num_train_epochs 10000000000000000 \
-  --cache_dir /ibex/user/hej0b/zhengyang/PseudoTrain/cache \
-  --output_dir /ibex/user/hej0b/zhengyang/PseudoTrain/tmp/mrpc/ \
-  --overwrite_output_dir > /ibex/user/hej0b/zhengyang/PseudoTrain/zhengyang_$time.log
+  --save_strategy no \
+  --cache_dir /home/tangzhengyang/PseudoTrain/cache \
+  --output_dir /home/tangzhengyang/PseudoTrain/tmp/mrpc/ \
+  --overwrite_output_dir > /home/tangzhengyang/PseudoTrain/zhengyang_$time.log
